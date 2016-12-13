866b83b7849fb9b98a86edb3890ebbedf88c7a46
poedevop <smart.training98@gmail.com>
2016-12-13 16:52:19 +0100

    first commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ def hello():
     return "hello world "
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 
 
 
